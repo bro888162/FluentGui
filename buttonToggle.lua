fc818f1bba7ebfa667eccb5e4c919d962a3a05f0
bro888162 <Sergemkrt@gmail.com>
2025-02-01 17:29:01 +0100

    Add files via upload

---
+++ buttonToggle.lua
@@ -0,0 +1,40 @@
+--creds Seven7
+local CoreGui = game:GetService("CoreGui")
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "Toggleui"
+ScreenGui.Parent = CoreGui
+
+local Toggle = Instance.new("TextButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.BackgroundTransparency = 0.5
+Toggle.AnchorPoint = Vector2.new(0, 0) -- Top-left alignment
+Toggle.Position = UDim2.new(0.25, 0, 0.05, 0)
+Toggle.Size = UDim2.new(0, 64, 0, 64)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = ""
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
+Toggle.TextSize = 18.000
+Toggle.Draggable = true
+
+local Corner = Instance.new("UICorner")
+Corner.CornerRadius = UDim.new(0.2, 0)
+Corner.Parent = Toggle
+
+local Image = Instance.new("ImageLabel")
+Image.Name = "Icon"
+Image.Parent = Toggle
+Image.Size = UDim2.new(1, 0, 1, 0)
+Image.BackgroundTransparency = 1
+Image.Image = "rbxassetid://135859317096442" 
+
+local Corner2 = Instance.new("UICorner")
+Corner2.CornerRadius = UDim.new(0.2, 0)
+Corner2.Parent = Image
+
+Toggle.MouseButton1Click:Connect(function()
+    if CoreGui:FindFirstChild("FluentGui") then
+        CoreGui.FluentGui.Enabled = not CoreGui.FluentGui.Enabled
+    end
+end)
