975853a1bd06a834aa2acdd27f7473832b21ca57
bro888162 <Sergemkrt@gmail.com>
2025-02-01 17:38:20 +0100

    Update buttonToggle.lua

--- buttonToggle.lua
+++ buttonToggle.lua
@@ -1,5 +1,9 @@
---creds Seven7
+-- creds Seven7
 local CoreGui = game:GetService("CoreGui")
+local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "Toggleui"
 ScreenGui.Parent = CoreGui
@@ -13,7 +17,7 @@ Toggle.AnchorPoint = Vector2.new(0, 0) -- Top-left alignment
 Toggle.Position = UDim2.new(0.25, 0, 0.05, 0)
 Toggle.Size = UDim2.new(0, 64, 0, 64)
 Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = ""
+Toggle.Text = "Shift"
 Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 18.000
 Toggle.Draggable = true
@@ -27,14 +31,12 @@ Image.Name = "Icon"
 Image.Parent = Toggle
 Image.Size = UDim2.new(1, 0, 1, 0)
 Image.BackgroundTransparency = 1
-Image.Image = "rbxassetid://135859317096442" 
+Image.Image = "rbxassetid://135859317096442"
 
 local Corner2 = Instance.new("UICorner")
 Corner2.CornerRadius = UDim.new(0.2, 0)
 Corner2.Parent = Image
 
 Toggle.MouseButton1Click:Connect(function()
-    if CoreGui:FindFirstChild("FluentGui") then
-        CoreGui.FluentGui.Enabled = not CoreGui.FluentGui.Enabled
-    end
+    UserInputService.InputBegan:Fire(Enum.KeyCode.LeftShift)
 end)
